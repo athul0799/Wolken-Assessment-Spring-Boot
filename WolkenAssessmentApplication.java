31130ce3d067a9f4582d2394b3d07441261028dd
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-11-22 23:18:39 +0530

    Create WolkenAssessmentApplication.java

---
+++ WolkenAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolkenAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolkenAssessmentApplication.class, args);
+	}
+
+}
