e5b90ef1f8ddd424d3caa61e8254391d8bc8d78b
athul0799 <64894900+athul0799@users.noreply.github.com>
2021-11-22 23:06:14 +0530

    Create UserDTO.java

---
+++ dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.example.serve.dto;
+
+
+	import lombok.Getter;
+	import lombok.NoArgsConstructor;
+	import lombok.Setter;
+
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	public class UserDTO {
+		private int userId;
+		private String firstName;
+		private String lastName;
+		private String designation;
+		private String code;
+		private long contact;
+		private String email;
+		private String country;
+		private String gender;
+		private String address;
+		
+		}
+
+	
+	
+
+	
+
